fc0ac2b2076ac67cee9774e55bb7ce80ef35a10a
schmitz1193 <schmitz1193@gmail.com>
2016-02-16 12:56:27 -0600

    added header view

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 'use strict';
-
-const app = require('express')();
+const express = require('express');
+const app = express();
+//const app = require('express')();
 //const bodyParser = require('body-parser');  //process for forms
 //const mongoose = require('mongoose');
-
+const path = require('path');
 //const routes = require('./routes/');  //slash as the end indicated you look in index first
-//const path = require('path');
+const sassMiddleware = require('node-sass-middleware');
 const PORT = process.env.PORT || 3000;  //set up environment port if none given default to 3000
+app.set('view engine', 'jade');  //set up engine to compile JADE
+
+
+//app.use(bodyParser.urlencoded({ extended: false }));  //urlencoded parse url
+//app.use(bodyParser.json()); //parse json
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use(routes);  //allows you to use the diferent routes
+
+//SASS set up
+app.use(sassMiddleware({
+  /* Options */
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true,
+  sourceMap: true
+}));
 
 app.locals.title = 'MARKET ON DEMAND';
 
 // respond with "hello investors" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello investors');
+app.get('/', (req, res) => {
+    res.render('index', {
+      msg: 'Welcome Investors!!!'
+    });
 });
 
 app.listen(PORT, () => {
